21975d3487433d1f48219ae65fdee7d43e16f842
Victor Souza <victor_souzas@yahoo.com.br>
2017-11-16 10:49:12 -0200

    Quick sort by password size

---
+++ Sorting/QuickByPasswordSize.java
@@ -0,0 +1,14 @@
+package Sorting;
+
+import Start.Senha;
+
+public class QuickByPasswordSize extends QuickSort<Senha> {
+    public QuickByPasswordSize(Senha[] vector) {
+        super(vector);
+    }
+
+    @Override
+    protected boolean greaterOrEqualThan(Senha x, Senha y) {
+        return x.getTamanho() >= y.getTamanho();
+    }
+}
