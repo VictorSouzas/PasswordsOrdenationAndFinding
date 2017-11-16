b9cff195b181f28ea66c4e0b9b954fcb16ae7947
Victor Souza <victor_souzas@yahoo.com.br>
2017-11-16 10:50:45 -0200

    Binary abstract class that uses to genTypes to do a binary search

---
+++ searching/Binary.java
@@ -0,0 +1,31 @@
+package searching;
+
+public abstract class Binary<T,G> {
+    protected T[] vector;
+    protected boolean flag;
+
+    public Binary(T[] vector) {
+        this.vector = vector;
+        this.flag = true;
+    }
+
+    protected int binarySearch(int min, int max, G value){
+        if(min > max)
+            return -1;
+        int center = min + (max - min) / 2;
+        if(equalsTo(vector[center],value))
+            return center;
+        if(greaterThan(vector[center], value))
+            return binarySearch(center+1, max, value);
+        if(lowerThan(vector[center], value))
+            return binarySearch(min, center-1, value);
+        return -1;
+    }
+
+    protected abstract boolean equalsTo(T x, G y);
+
+    protected abstract boolean greaterThan(T x, G y);
+
+    protected abstract boolean lowerThan(T x, G y);
+
+}
